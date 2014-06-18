0265a162a69d3c2c7c9c681decfb28f20d017757
phitchcock <phitchcock@me.com>
2014-06-17 21:36:18 -0700

    starting custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "pages#index"
 
+  #get "/404", :to => "errors#not_found"
+  #get "/422", :to => "errors#unacceptable"
+  #get "/500", :to => "errors#internal_error"
+
+
   devise_for :users
 
   resources :charges, only: [:new, :create]
