8c7aee6349fa9a9e828299ee0b607ff124e7a2b8
phitchcock <phitchcock@me.com>
2014-06-18 10:37:37 -0700

    updated navbar, user show and user index views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @wikis = current_user.wikis.paginate(page: params[:page], per_page: 3)
+    @wikis = @user.wikis.paginate(page: params[:page], per_page: 5)
   end
 
   def update
