61bcd51e8bd589ce0588fded380ac3f63a342c41
phitchcock <phitchcock@me.com>
2014-06-19 23:40:29 -0700

    login register fb button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,42 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
+# require 'faker'
 
-5.times do
-  user = User.new(
-    email:    Faker::Internet.email,
-    password: Faker::Lorem.characters(10)
-  )
-  user.save
-end
-users = User.all
+# 5.times do
+#   user = User.new(
+#     email:    Faker::Internet.email,
+#     password: Faker::Lorem.characters(10)
+#   )
+#   user.save
+# end
+# users = User.all
 
-50.times do 
-  idea.create(
-    title: Faker::Commerce.product_name,
-    body: Faker::Lorem.paragraph
-    )
-end
-ideas = idea.all
+# 50.times do 
+#   idea.create(
+#     title: Faker::Commerce.product_name,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+# ideas = idea.all
 
-100.times do 
-  screen.create(
-    idea: ideas.sample,
-    body: Faker::Lorem.paragraph
-    )
-end
-screens = screen.all
+# 100.times do 
+#   screen.create(
+#     idea: ideas.sample,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+# screens = screen.all
 
-200.times do
-  Comment.create(
-    user: users.sample,
-    screen: screens.sample,
-    info: Faker::Lorem.paragraph
-    )
-end
+# 200.times do
+#   Comment.create(
+#     user: users.sample,
+#     screen: screens.sample,
+#     info: Faker::Lorem.paragraph
+#     )
+# end
 
-puts "Seed finished"
-puts "#{idea.count} ideas created"
-puts "#{screen.count} screens created"
-puts "#{Comment.count} comments created"
+# puts "Seed finished"
+# puts "#{idea.count} ideas created"
+# puts "#{screen.count} screens created"
+# puts "#{Comment.count} comments created"
