ec1422107c34de50a7cce41e9b773daf97f5cade
phitchcock <phitchcock@me.com>
2014-06-20 00:13:55 -0700

    added name to users, updated views to reflect

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,8 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      #user.name = auth.info.name   # assuming the user model has a name
-      #user.avatar = auth.info.image # assuming the user model has an image
+      user.name = auth.info.name   # assuming the user model has a name
+      user.avatar = auth.info.avatar # assuming the user model has an image
     end
   end
 
