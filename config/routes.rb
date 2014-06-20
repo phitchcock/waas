da5ff223605d1c0e787a3c50af36e9c0e492de26
phitchcock <phitchcock@me.com>
2014-06-19 21:50:50 -0700

    fb login heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   #get "/422", :to => "errors#unacceptable"
   #get "/500", :to => "errors#internal_error"
 
-  devise_scope :user do
-    #get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
